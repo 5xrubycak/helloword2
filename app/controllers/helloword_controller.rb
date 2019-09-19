88127855e36504a6c42e6c114db0336f1a638ebb
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-09-19 17:45:27 +0800

    套用bootstrap & 建立HelloWordController

--- app/controllers/helloword_controller.rb
+++ app/controllers/helloword_controller.rb
@@ -1,2 +1,14 @@
 class HellowordController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @setbox = Setbox.new
+    @card = Card.new
+  end
+
+  def create
+    render html: 123
+  end
 end
