7eaacc2b183fdc577405b34fd9dc035d06087492
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-09-21 17:15:59 +0800

    新增setboxes的user_id欄位& 新增學習集可以寫進資料庫

--- app/controllers/setboxes_controller.rb
+++ app/controllers/setboxes_controller.rb
@@ -9,12 +9,12 @@ class SetboxesController < ApplicationController
 
   def create
     setbox_clean_params = params.require(:setbox).permit(:title, cards_attributes: [:id, :card_word, :card_def ,:_destroy])
-    @setbox = Setbox.new(setbox_clean_params)
+    @setbox = current_user.setboxes.build(setbox_clean_params)
     
     if @setbox.save
       redirect_to root_path, notice: "新增setbox成功" 
     else
-      render :new
+      render :new, notice: "error"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_143714) do
+ActiveRecord::Schema.define(version: 2019_09_21_091116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_09_19_143714) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_setboxes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_09_19_143714) do
   end
 
   add_foreign_key "cards", "setboxes"
+  add_foreign_key "setboxes", "users"
 end
