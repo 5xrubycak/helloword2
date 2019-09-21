c6dcb01a139e0245a0120ee2573ee7d814d22b25
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-09-21 23:41:05 +0800

    美化版型、簡化整理Controller程式碼

--- app/controllers/setboxes_controller.rb
+++ app/controllers/setboxes_controller.rb
@@ -1,4 +1,6 @@
 class SetboxesController < ApplicationController
+
+  before_action :find_setbox, only: [:show, :edit, :update]
   def index
     @setbox = Setbox.all
   end
@@ -8,8 +10,7 @@ class SetboxesController < ApplicationController
   end
 
   def create
-    setbox_clean_params = params.require(:setbox).permit(:title, cards_attributes: [:id, :card_word, :card_def ,:_destroy])
-    @setbox = current_user.setboxes.build(setbox_clean_params)
+    @setbox = current_user.setboxes.build(setbox_params)
     
     if @setbox.save
       redirect_to root_path, notice: "新增setbox成功" 
@@ -19,18 +20,13 @@ class SetboxesController < ApplicationController
   end
 
   def show
-    @setbox = Setbox.find_by(id: params[:id])
   end
 
   def edit
-    @setbox = Setbox.find_by(id: params[:id])
   end
 
   def update
-    @setbox = Setbox.find_by(id: params[:id])
-    setbox_clean_params = params.require(:setbox).permit(:title, cards_attributes: [:id, :card_word, :card_def ,:_destroy])
-
-    if @setbox.update(setbox_clean_params)
+    if @setbox.update(setbox_params)
       redirect_to root_path, notice: "更新setbox成功"
     else
       render :edit
@@ -41,5 +37,15 @@ class SetboxesController < ApplicationController
     @setbox = Setbox.find_by(id: params[:id])
     @setbox.destroy
     redirect_to root_path, notice: "刪除資料"
+  end
+
+  private
+
+  def setbox_params
+    setbox_clean_params = params.require(:setbox).permit(:title, cards_attributes: [:id, :card_word, :card_def ,:_destroy])
+  end
+
+  def find_setbox
+    @setbox = Setbox.find_by(id: params[:id])
   end
 end
