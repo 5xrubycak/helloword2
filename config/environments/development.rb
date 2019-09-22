a96691bc82f356a126766bdc69e86300c82ce9f1
Kevin Wang <kevin0117@gmail.com>
2019-09-22 15:00:27 +0800

    013_安裝ActionMailer & 設定Mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['smtp_address'],
+    port: 587,
+    domain: '5xruby.tw',
+    user_name: ENV['smtp_username'],
+    password: ENV['smtp_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
