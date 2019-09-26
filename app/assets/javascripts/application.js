70b85b35328ea94d0aea6f5ebbf46aabfd3dff53
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-09-26 09:16:26 +0800

    015 分享按鈕點擊可達成copy文字效果

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,20 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// 分享功能的JS
+function urlFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("urltext");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+// ----------
