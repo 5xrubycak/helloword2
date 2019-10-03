b64364f47ae5bd87b705461430870b17010e6f95
Kevin Wang <kevin0117@gmail.com>
2019-10-03 12:27:11 +0800

    000_寄信功能上線更改設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://stark-plains-19533.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['smtp_address'],
+    port: 587,
+    domain: 'postmaster@helloword.space',
+    user_name: ENV['smtp_username'],
+    password: ENV['smtp_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
