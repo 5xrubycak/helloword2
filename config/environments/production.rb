f2f928e8cbea7bb81a4f4487bb020b8a8c5212b8
Kevin Wang <kevin0117@gmail.com>
2019-10-03 13:02:29 +0800

    000_修正嘗試1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :production
   config.action_mailer.default_url_options = { host: 'https://stark-plains-19533.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
