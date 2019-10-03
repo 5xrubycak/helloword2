564b4ea57e80ed9d48d17bc62aab3bf70d364fef
Kevin Wang <kevin0117@gmail.com>
2019-10-03 13:26:24 +0800

    000_寄信功能修正＿01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "helloword2_#{Rails.env}"
-
+  # config.action_mailer.delivery_method = :test
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :production
