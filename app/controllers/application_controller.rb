c21cd20618d4d19c849895d973ec11e67f6d3c2e
Apple chen <appletabby@gmail.com>
2019-10-11 15:34:28 +0800

    044_新增i18n英文版

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   # after_action :store_action
   protect_from_forgery with: :exception
-  
   before_action :set_locale
+  around_action :switch_locale
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+ 
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
   
   def default_url_options(options = {})
