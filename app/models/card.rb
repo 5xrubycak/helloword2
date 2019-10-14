ffaa80b5a07e30a6b3147410bb3f451228621508
Apple chen <appletabby@gmail.com>
2019-10-14 19:11:06 +0800

    052_單字卡輸入欄位空白、長度驗證

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :setbox
+
+  validates :card_word, :card_def, presence: true
+  validates :card_word, :card_def, length: { maximum: 50,
+    too_long: "最長不能超過50個字喔！" }
 end
--- app/models/setbox.rb
+++ app/models/setbox.rb
@@ -3,6 +3,10 @@ class Setbox < ApplicationRecord
   has_many :cards, inverse_of: :setbox, :dependent => :destroy
   accepts_nested_attributes_for :cards, reject_if: :all_blank, allow_destroy: true
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 50,
+    too_long: "最長不能超過50個字喔！" }
+
   def self.search(search) 
     if search
       where(['title LIKE ? OR cards.card_word LIKE ? OR cards.card_def LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"]) 
@@ -11,15 +15,7 @@ class Setbox < ApplicationRecord
     end
   end
 
-  def self.write(write) 
-    
-    if write
-      where("cards.card_def = ?", params[:write])
-      redirect_to pullreq_setboxes_path, notice: "完全正確！" 
-    else
-      all
-    end
-  end
+
 end
 
 
